b3534c513f866a0bf399f5b9f41c83460e813eaa
Fabio Lex Castro <fabioalexandercastro@gmail.com>
2023-02-25 09:51:33 -0500

    Update 01 aed ideam.R

--- 01 aed ideam.R
+++ 01 aed ideam.R
@@ -15,6 +15,23 @@ options(scipen = 999, warn = -1)
 dpto <- vect('G:/D/data/IGAC/MGN2018_MPIO_POLITICO/MGN_MPIO_POLITICO.shp')
 mpio <- vect('G:/D/data/IGAC/MGN2018_DPTO_POLITICO/MGN_DPTO_POLITICO.shp')
 
+# Study area
+dpto.zone <- dpto[dpto$DPTO_CNMBR %in% c('VICHADA', 'META', 'CASANARE', 'ARAUCA'),]
+mpio.zone <- mpio[mpio$DPTO_CNMBR %in% c('VICHADA', 'META', 'CASANARE', 'ARAUCA'),]
+
+# Write the study area
+dir_create('data/gpk')
+writeVector(dpto.zone, 'data/gpk/dpto_zone.gpkg')
+writeVector(mpio.zone, 'data/gpk/mpio_zone.gpkg')
+
+# IDEAM Data
+
+
+
+
+
+
+
 
 
 
