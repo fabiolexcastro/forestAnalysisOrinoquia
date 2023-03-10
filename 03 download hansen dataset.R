4d17ed5246e3a1240db1d0022374558389f1242b
Fabio Lex Castro <fabioalexandercastro@gmail.com>
2023-03-09 20:31:05 -0500

    Update 03 download hansen dataset.R

--- 03 download hansen dataset.R
+++ 03 download hansen dataset.R
@@ -27,12 +27,36 @@ download_tiles(tiles = tles, output_folder = 'data/tif/hasen/raw_2021', dataset 
 # To extract by mask ------------------------------------------------------
 extr <- extract_gfc(aoi = dpto, data_folder = 'data/tif/hansen/raw_2021', dataset = 'GFC-2021-v1.9')
 
+loss <- extr[[2]]
+
 # Binarization (Forest/No Forest)
 thrs <- threshold_gfc(gfc = extr)
+raster::writeRaster(x = thrs, filename = 'data/tif/hansen/thr_2021/threshold_v1.tif', overwrite = T)
 
+# Masking -----------------------------------------------------------------
+thrs <- terra::rast(thrs)
+thrs <- terra::mask(thrs, vect(dpto))
 
+terra::writeRaster(x = thrs, filename = 'data/tif/hansen/thr_2021/threshold_v2.tif')
 
+# rall <- c(r1, r2, r3) # Make a stack (IDEAM, i.e.,)
 
+loss <- thrs[[2]]
+bqnb <- list()
+
+bqnb[[1]] <- thrs[[1]]
+bqnb[[1]][which.lyr(loss == 1)] <- 0
+
+bqnb[[1]] <- thrs[[1]]
+
+for(i in 1:21){
+  
+  cat('Processing: ', i + 2000, '\n')
+  bqnb[[i]]
+  
+  
+  
+}
 
 
 
