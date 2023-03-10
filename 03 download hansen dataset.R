afa9a06d632f0b33b3038ff71a4c4cf34c1e0eca
Fabio Lex Castro <fabioalexandercastro@gmail.com>
2023-03-09 19:56:00 -0500

    Update 03 download hansen dataset.R

--- 03 download hansen dataset.R
+++ 03 download hansen dataset.R
@@ -21,8 +21,17 @@ mpio <- vect('data/gpk/mpio_zone.gpkg')
 # To download -------------------------------------------------------------
 dpto <- st_as_sf(dpto)
 tles <- calc_gfc_tiles(aoi = dpto)
-download_tiles(tiles = tles, output_folder = 'data/tif/ideam/hansen_raw_2021', dataset = 'GFC-2021-v1.9')
+download_tiles(tiles = tles, output_folder = 'data/tif/hasen/raw_2021', dataset = 'GFC-2021-v1.9')
 ?download_tiles
+
+# To extract by mask ------------------------------------------------------
+extr <- extract_gfc(aoi = dpto, data_folder = 'data/tif/hansen/raw_2021', dataset = 'GFC-2021-v1.9')
+
+# Binarization (Forest/No Forest)
+thrs <- threshold_gfc(gfc = extr)
+
+
+
 
 
 
